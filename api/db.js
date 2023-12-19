0074676331f1febef314185c4d06caf48adaffa5
Aleksandar Stoyanov <alexbsbg@gmail.com>
2023-12-19 20:59:40 +0200

    fixed database info

--- api/db.js
+++ api/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 // Create a connection pool
 const pool = mysql.createPool({
-    host: 'your_mysql_host',
-    user: 'your_mysql_user',
-    password: 'your_mysql_password',
-    database: 'your_mysql_database',
+    host: '3306',
+    user: 'root',
+    password: '15948264815926q6',
+    database: 'mysql',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0
